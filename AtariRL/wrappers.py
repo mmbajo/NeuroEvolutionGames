b0a6f9d09dc23cf56315ac305f8e5c6f7ad62dfd
mmbajo <markarjaybajo@gmail.com>
2020-01-02 14:37:36 +0900

    add yet another class BufferWrapper

--- AtariRL/wrappers.py
+++ AtariRL/wrappers.py
@@ -53,7 +53,7 @@ class MaxAndSkipEnv(gym.Wrapper):
 
 class ProcessFrame84(gym.ObservationWrapper):
     def __init__(self, env = None):
-        super(ProcessFrame84).__init__(env)
+        super(ProcessFrame84, self).__init__(env)
         self.observation_space = gym.spaces.Box(low = 0, high = 255, shape = (84, 84, 1), dtype = np.uint8)
 
         def observaton(self, obs):
@@ -75,7 +75,7 @@ class ProcessFrame84(gym.ObservationWrapper):
 
 class BufferWrapper(gym.ObservationWrapper):
     def __init__(self, env, n_steps, dtype = np.float32):
-        super(BufferWrapper).__init__(env)
+        super(BufferWrapper, self).__init__(env)
         self.dtype = dtype
         old_space = env.observation_space
         self.observation_space = gym.spaces.Box(old_space.low.repeat(n_steps, axis = 0), old_space.high.repeat(n_steps, axis = 0), dtype = dtype)
@@ -89,3 +89,25 @@ class BufferWrapper(gym.ObservationWrapper):
             self.buffer[-1] = observation
             return self.buffer
 
+class ImageToPyTorch(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        self.observation_space = gym.spaces.Box(low = 0.0, high = 1.0, shape = (old_shape[-1], old_shape[0], old_shape[1]), dtype = np.float32)
+
+        def observation(self, observation):
+            return np.moveaxis(observation, 2, 0)
+        
+class ScaledFloatFrame(gym.ObservationWrapper):
+    def observation(self, obs):
+        return np.array(obs).astype(np.float32) / 255.0
+
+
+def make_env(env_name):
+    env = gym.make(env_name)
+    env = MaxAndSkipEnv(env)
+    env = FireResetEnv(env)
+    env = ProcessFrame84(env)
+    env = ImageToPyTorch(env)
+    env = BufferWrapper(env, 4)
+    return ScaledFloatFrame(env)
