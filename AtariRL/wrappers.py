9098ec8a5a3e7da6f5826d0e5b4fcef35cd0878d
mmbajo <markarjaybajo@gmail.com>
2020-01-02 14:06:26 +0900

    add yet another class BufferWrapper

--- AtariRL/wrappers.py
+++ AtariRL/wrappers.py
@@ -73,3 +73,19 @@ class ProcessFrame84(gym.ObservationWrapper):
             x_t = np.reshape(x_t, [84, 84, 1])
             return x_t.astype(np.uint8)
 
+class BufferWrapper(gym.ObservationWrapper):
+    def __init__(self, env, n_steps, dtype = np.float32):
+        super(BufferWrapper).__init__(env)
+        self.dtype = dtype
+        old_space = env.observation_space
+        self.observation_space = gym.spaces.Box(old_space.low.repeat(n_steps, axis = 0), old_space.high.repeat(n_steps, axis = 0), dtype = dtype)
+
+        def reset(self):
+            self.buffer = np.zeros_like(self.observation_space.low, dtype = self.dtype)
+            return self.observation(self.env.reset())
+        
+        def observation(self, observation):
+            self.buffer[:-1] = self.buffer[1:]
+            self.buffer[-1] = observation
+            return self.buffer
+
