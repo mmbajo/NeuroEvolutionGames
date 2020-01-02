c6711a160dced243703271107073e925d92b2ab2
mmbajo <markarjaybajo@gmail.com>
2020-01-02 16:01:21 +0900

    add dqn pong

--- AtariRL/dqn_model.py
+++ AtariRL/dqn_model.py
@@ -21,3 +21,11 @@ class DQN(nn.Module):
                                 nn.ReLU(),
                                 nn.Linear(512, n_actions)
                                )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+    
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
---
+++ AtariRL/dqn_pong.py
@@ -0,0 +1,25 @@
+import wrappers
+import dqn_model
+
+import argparse
+import time
+import numpy as np
+import collections
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from tensorboardX import SummaryWriter
+
+DEFAULT_ENV_NAME = 'PongNoFrameSkip-v4'
+MEAN_REWARD_BOUND = 19.5
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+REPLAY_SIZE = 10000
+LEARNING_RATE = 1e-4
+SYNC_TARGET_FRAMES = 1000
+REPLAY_START_SIZE = 10000
+
+EPSILON_DECAY_LASTFRAME = 10**5
